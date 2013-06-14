a5968c427da35755502c21b7615ac3fe6f9e5bdd
Simon Wheatley <simon@sweetinteraction.com>
2013-06-14 11:25:22 +0100

    Remove cruft from authentication class.

--- class.twitter-authentication.php
+++ class.twitter-authentication.php
@@ -60,7 +60,6 @@ class TT_Twitter_Authentication {
 	 **/
 	public function __construct() {
 
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'load-settings_page_tt_auth', array( $this, 'load_settings' ) );
 		add_action( 'admin_notices', array( $this, 'admin_notices' ) );
@@ -78,17 +77,6 @@ class TT_Twitter_Authentication {
 		if ( ! $this->creds[ 'authenticated' ] )
 			return $creds;
 		return $this->creds;
-	}
-
-	public function admin_init() {
-		$this->maybe_upgrade();
-		// Handy couple of lines to reset all the tweet collection
-//		$this->init_oauth();
-//		$this->oauth->delete_property( 'last_mention_id' );
-//		delete_option( 'twtwchr_queued_mentions' );
-//		var_dump( "Done" );
-//		exit;
-//		$this->queue_new_mentions();
 	}
 	
 	public function admin_menu() {
@@ -189,62 +177,11 @@ class TT_Twitter_Authentication {
 			printf( '<div class="error"><p>%s</p></div>', sprintf( __( 'Authorisation with Twitter was <strong>not</strong> completed.', 'twitter-tracker' ), $this->creds[ 'screen_name' ] ) );
 	}	
 	
-	public function queue_new_mentions() {
-		$this->init_oauth();
-
-		if ( ! $users = $this->oauth->get_users() )
-			return;
-		
-		foreach ( $users as $user_id => & $user ) {
-			$args = array( 'include_entities' => 'true', 'count' => 100 );
-			if ( isset( $user[ 'last_mention_id' ] ) && $user[ 'last_mention_id' ] ) {
-				$args[ 'since_id' ] = $user[ 'last_mention_id' ];
-			}
-			if ( $mentions = $this->oauth->get_mentions( $user_id, $args ) ) {
-				$queued_mentions = (array) get_option( 'twtwchr_queued_mentions', array() );
-				foreach ( $mentions as & $mention ) {
-					// error_log( "TW: Queue $mention->id_str" );
-					array_unshift( $queued_mentions, $mention );
-				}
-				update_option( 'twtwchr_queued_mentions', $queued_mentions );
-				$last_mention = array_shift( $mentions );
-				$this->oauth->set_user_property( $user_id, 'last_mention_id', $last_mention->id_str );
-			}
-		}
-		$this->process_mentions();
-	}
-	
-	public function queue_new_tweets() {
-		$this->init_oauth();
-
-		if ( ! $users = $this->oauth->get_users() )
-			return;
-		
-		foreach ( $users as $user_id => & $user ) {
-			$args = array( 'include_entities' => 'true', 'include_rts' => 'true', 'contributor_details' => 'true', 'count' => 100 );
-			if ( isset( $user[ 'last_tweet_id' ] ) && $user[ 'last_tweet_id' ] ) {
-				$args[ 'since_id' ] = $user[ 'last_tweet_id' ];
-			}
-			if ( $tweets = $this->oauth->get_tweets( $user_id, $args ) ) {
-				$queued_tweets = (array) get_option( 'twtwchr_queued_tweets', array() );
-				foreach ( $tweets as & $tweet ) {
-					array_unshift( $queued_tweets, $tweet );
-				}
-				update_option( 'twtwchr_queued_tweets', $queued_tweets );
-				$last_tweet = array_shift( $tweets );
-				$this->oauth->set_user_property( $user_id, 'last_tweet_id', $last_tweet->id_str );
-			}
-		}
-		$this->process_tweets();
-	}
-	
 	// CALLBACKS
 	// =========
 	
 	public function settings() {
 
-
-		var_dump( $this->creds );
 		if ( $this->creds[ 'authenticated' ] ) {
 			$vars = array();
 			$vars[ 'authenticated' ] = $this->creds[ 'authenticated' ];
@@ -261,8 +198,6 @@ class TT_Twitter_Authentication {
 	// =======
 
 	public function view_unauthenticate( $vars ) {
-		var_dump( $vars );
-		
 		extract( $vars );
 ?>
 
@@ -374,165 +309,6 @@ class TT_Twitter_Authentication {
 	public function is_authentication_response() {
 		return isset( $_GET[ 'oauth_token' ] );
 	}
-	
-	public function process_tweets() {
-		// Try to give outselves a 4 minute execution time to play with,
-		// bearing in mind that the Cron job is every five minutes.
-		set_time_limit( 4*60 );
-		while( $queued_tweets = get_option( 'twtwchr_queued_tweets', array() ) ) {
-			$tweet = array_pop( $queued_tweets );
-
-			$tweet->html_text = $tweet->text;
-			$is_protected = $tweet->user->protected;
-			$is_reply = (bool) ( isset( $tweet->in_reply_to_status_id_str ) && $tweet->in_reply_to_status_id_str );
-			$is_retweet = $tweet->retweeted;
-
-			$hashtags = wp_list_pluck( $tweet->entities->hashtags, 'text' );
-			$tweet = $this->make_hashtags_links( $tweet, $hashtags );
-
-			$user_mentions = $this->extract_user_mentions( $tweet );
-			$tweet = $this->make_user_mentions_links( $tweet, $user_mentions );
-
-			$urls = wp_list_pluck( $tweet->entities->urls, 'expanded_url' );
-			$tweet = $this->make_urls_links( $tweet );
-
-			do_action( 'twtwchr_tweet', $tweet, $tweet->id_str, $is_reply, $is_retweet, $is_protected, $hashtags, $user_mentions, $urls );
-
-			update_option( 'twtwchr_queued_tweets', $queued_tweets );
-		}
-//		exit;
-	}
-	
-	public function process_mentions() {
-		// Try to give outselves a 4 minute execution time to play with,
-		// bearing in mind that the Cron job is every five minutes.
-		set_time_limit( 4*60 );
-		while( $queued_mentions = get_option( 'twtwchr_queued_mentions', array() ) ) {
-			$tweet = array_pop( $queued_mentions );
-
-			$tweet->html_text = $tweet->text;
-			$is_protected = $tweet->user->protected;
-
-			$hashtags = wp_list_pluck( $tweet->entities->hashtags, 'text' );
-			$tweet = $this->make_hashtags_links( $tweet, $hashtags );
-
-			$user_mentions = $this->extract_user_mentions( $tweet );
-			$tweet = $this->make_user_mentions_links( $tweet, $user_mentions );
-
-			$urls = wp_list_pluck( $tweet->entities->urls, 'expanded_url' );
-			$tweet = $this->make_urls_links( $tweet );
-			
-			do_action( 'twtwchr_mention', $tweet, $tweet->id_str, $is_protected, $hashtags, $user_mentions, $urls );
-
-			update_option( 'twtwchr_queued_mentions', $queued_mentions );
-			unset( $queued_mentions );
-		}
-	}
-	
-	public function make_hashtags_links( $tweet, $hashtags ) {
-		// Make links from the hashtags in the tweet text
-		$search = array();
-		$replace = array();
-		foreach ( $hashtags as & $s ) {
-			$hashtag = "#$s";
-			$args = array( 'q' => rawurlencode( $hashtag ) );
-			$hashtag_url = add_query_arg( $args, 'https://twitter.com/search/realtime/' );
-			$search[] = $hashtag;
-			$replace[] = '<a href="' . esc_url( $hashtag_url ) . '">' . esc_html( $hashtag ) . '</a>';
-		}
-		$tweet->html_text = str_replace( $search, $replace, $tweet->html_text );
-		return $tweet;
-	}
-
-	public function extract_user_mentions( $tweet ) {
-		$user_mentions = array();
-		foreach ( $tweet->entities->user_mentions as & $user_mention )
-			$user_mentions[ $user_mention->screen_name ] = $user_mention->name;
-		return $user_mentions;
-	}
-	
-	public function make_user_mentions_links( $tweet, $user_mentions ) {
-		// Make links from the user mentions in the tweet text
-		$search = array();
-		$replace = array();
-		foreach ( $user_mentions as $screen_name => $name ) {
-			$user = "@$screen_name";
-			$user_url = "https://twitter.com/$screen_name";
-			$search[] = $user;
-			$replace[] = '<a href="' . esc_url( $user_url ) . '" title="' . esc_attr( $name ) . '">' . esc_html( $user ) . '</a>';
-		}
-		$tweet->html_text = str_replace( $search, $replace, $tweet->html_text );
-		return $tweet;
-	}
-	
-	public function make_urls_links( $tweet ) {
-		// Make links from the URLs in the tweet text
-		$search = wp_list_pluck( $tweet->entities->urls, 'url' );
-		$replace = array();
-		foreach ( $tweet->entities->urls as & $url )
-			$replace[] = '<a href="' . esc_url( $url->expanded_url ) . '">' . esc_html( $url->display_url ) . '</a>';
-		$tweet->html_text = str_replace( $search, $replace, $tweet->html_text );
-		return $tweet;
-	}
-	
-	/**
-	 * We cannot sanitise some ID strings by converting to integers, as 
-	 * they will overflow 32 bit systems and corrupt data. This method
-	 * sanitises without converting to ints.
-	 * 
-	 * @param string $id_str An integer ID represented as a string to be sanitised
-	 * @return string A sanitised integer ID 
-	 */
-	public function sanitise_id_str( $id_str ) {
-		$id_str = preg_replace( '/[^\d]/', '', (string) $id_str );
-		return $id_str;
-	}
-	
-	/**
-	 * Checks the DB structure is up to date.
-	 *
-	 * @return void
-	 * @author Simon Wheatley
-	 **/
-	function maybe_upgrade() {
-		return;
-		global $wpdb;
-		$option_name = 'twtwchr_version';
-		$version = get_option( $option_name, 0 );
-
-		if ( $version == $this->version )
-			return;
-
-		delete_option( "{$option_name}_running", true, null, 'no' );
-		if ( $start_time = get_option( "{$option_name}_running", false ) ) {
-			$time_diff = time() - $start_time;
-			// Check the lock is less than 30 mins old, and if it is, bail
-			if ( $time_diff < ( 60 * 30 ) ) {
-				error_log( "Tweet Watcher: Existing update routine has been running for less than 30 minutes" );
-				return;
-			}
-			error_log( "Tweet Watcher: Update routine is running, but older than 30 minutes; going ahead regardless" );
-		} else {
-			update_option( "{$option_name}_running", time(), null, 'no' );
-		}
-
-		if ( $version < 2 ) {
-			wp_clear_scheduled_hook( 'twtwchr_queue_mentions' );
-			wp_schedule_event( time(), 'twtwchr_check_interval', 'twtwchr_queue_mentions' );
-			error_log( "Tweet Watcher: Setup cron job for mentions." );
-		}
-
-		if ( $version < 3 ) {
-			wp_clear_scheduled_hook( 'twtwchr_queue_new_tweets' );
-			wp_schedule_event( time(), 'twtwchr_check_interval', 'twtwchr_queue_new_tweets' );
-			error_log( "Tweet Watcher: Setup cron job for new tweets." );
-		}
-
-		update_option( $option_name, $this->version );
-		delete_option( "{$option_name}_running", true, null, 'no' );
-		error_log( "Tweet Watcher: Done upgrade" );
-	}
-
 	
 }
 
